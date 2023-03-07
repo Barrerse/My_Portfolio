55a2eb9abd1138863a26eb898cff0afab0e94c83
Barrerse <sebastianbarrera@live.com>
2023-03-07 13:02:13 -0500

    Added boilerplate code for routes and called the routes on the app.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./index.css";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Project from "./Project";
+
 
 function App() {
   return (
-	<div className="App">
-	  <h1
-	  >My React App
-	</h1>
-	</div>
+	<>	
+	<Routes>
+		<Route path="/" element={<Home />} />	
+		<Route path="/about" element={<About />} />
+		<Route path="/contact" element={<Contact />} />
+		<Route path="/project" element={<Project />} />
+
+	</Routes>
+	</>
   );
 }
 
