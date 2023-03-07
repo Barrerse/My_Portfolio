33b671487f5d09dd3ea6d523e8b574bbc6a77218
Barrerse <sebastianbarrera@live.com>
2023-03-07 11:29:42 -0500

    installed some dependancies and rewrote index and app

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
-import "./App.css";
-import React, { useState } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-// import About from "./components/About";
-// import Resume from "./components/Resume";
+import React from "react";
+import "./index.css";
 
 function App() {
-	const [currentTab, setCurrentTab] = useState("about");
-
-	// This function checks to see which tab is selected and then generates the appropriate tab.
-	const renderTab = () => {
-		switch (currentTab) {
-			// case "about":
-			// 	return <About />;
-			// case "portfolio":
-			// 	return <Portfolio />;
-			// case "contact":
-			// 	return <Contact />;
-			// case "resume":
-			// 	return <Resume />;
-			// default:
-			// 	return null;
-		}
-	};
-
-	return (
-		<div>
-			<div className="mobile-header">
-				<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
-			</div>
-			<div>
-				<main>{renderTab()}</main>
-			</div>
-			<div>
-				<Footer></Footer>
-			</div>
-		</div>
-	);
+  return (
+	<div className="App">
+	  <h1>My React App</h1>
+	</div>
+  );
 }
+
 
 export default App;
