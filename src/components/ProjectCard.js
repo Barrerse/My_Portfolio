1c294d5e373a590c2587bafe5ca7ca60827310c7
Barrerse <sebastianbarrera@live.com>
2023-03-08 01:51:14 -0500

    Finalized the cards. Created a WorkCardData file that I used as an API to import the data to Project Card as props which then got imported to work.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,21 @@
-import "./ProjectCard.css";
+import "./ProjectCard.css"
 
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
   return (
-    <div>ProjectCard</div>
+    <div className="project-card">
+          <img src={props.imgsrc} alt="First Project"/>
+          <h2 className="project-title">{props.title}</h2>
+          <div className="project-details">
+            <p> {props.text}</p>
+            <div className="pro-btns">
+                <NavLink to={props.view} className="btn">View</NavLink>
+                <NavLink to={props.repo} className="btn">Repo</NavLink>
+            </div>
+        </div>
+        </div>
   )
 }
 
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,12 +3,17 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Hero2 from '../components/Hero2'
+// import ProjectCard from '../components/ProjectCard'
+import Work from '../components/Work'
+
 
 const Project = () => {
   return (
     <div>
         <Navbar/>
         <Hero2 heading="PROJECTS" text="Some of my recent work"/>
+        {/* <ProjectCard/> */}
+        <Work/>
         <Footer/> 
     </div>
   )
