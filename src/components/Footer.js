6cdefa7f1fc392d398751053f9ac4c3813150167
Barrerse <sebastianbarrera@live.com>
2023-03-07 19:56:36 -0500

    added styling to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,8 @@ import { FaInstagram, FaLocationArrow, FaMailBulk, FaLinkedin, FaGithub } from "
 const Footer = () => {
   return (
     <div className="footer">
-        <div className="footer__container">
-            <div className="footer__left">
+        <div className="footer-container">
+            <div className="footer-left">
                 <div className="location">
                     <FaLocationArrow size={25} style={{color: "#fff", marginRight: "2rem"}}/>
                     <div>
@@ -20,7 +20,7 @@ const Footer = () => {
                 <h4><FaMailBulk size={25} style={{color: "#fff", marginRight: "2rem"}}/>sebastianbarrera@live.com</h4>
                 </div>
             </div>
-            <div className="footer__right">
+            <div className="footer-right">
                 <h4>Follow my journey on social media</h4>
                 <p> Please feel free to reach out to me so that we can connect and learn more about each other. I believe that by collaborating on projects, we can continuously improve together and achieve our shared goals.</p>
                 <div className="socials">
