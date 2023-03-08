d8cdf8bf5b6924a0e2aaf95007fd74014c1803b5
Barrerse <sebastianbarrera@live.com>
2023-03-07 22:29:18 -0500

    Added Hero2 to all routes

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Hero2 from '../components/Hero2'
 
 const About = () => {
   return (
     <div> 
         <Navbar/> 
+        <Hero2 heading="ABOUT" text="Tech enthusiast, avid traveler, life-long learner"/>
         <Footer/> 
     </div>
   )
